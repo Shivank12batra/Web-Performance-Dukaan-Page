6b926f76d5f5b30eb08e9253e78fc1073c6df183
Shivank12batra <Shivank56batra@gmail.com>
2024-02-11 16:51:57 +0530

    signed commit check

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,35 @@
-import Navbar from '@/components/Navbar'
-import Hero from '@/components/Hero'
-import Brands from '@/components/Brands'
-import Features from '@/components/Features'
-import Tools from '@/components/Tools'
-import Themes from '@/components/Themes'
-import Plugins from '@/components/Plugins'
-import Globe from '@/components/Globe'
-import Blogs from '@/components/Blogs'
-import Reviews from '@/components/Reviews'
-import Selling from '@/components/Selling'
-import Footer from '@/components/Footer'
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import Brands from "@/components/Brands";
+import Features from "@/components/Features";
+import Tools from "@/components/Tools";
+import Themes from "@/components/Themes";
+import Plugins from "@/components/Plugins";
+import Globe from "@/components/Globe";
+import Blogs from "@/components/Blogs";
+import Reviews from "@/components/Reviews";
+import Selling from "@/components/Selling";
+import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
     <div>
-      <Navbar/>
-       <Hero/>
-       <Brands/>
-       <Features/>
-       <Tools/>
-       <Themes/>
-       <Plugins/>
-       <Globe/>
-       <Reviews/>
-       <Blogs/>
-       <Selling/>
-       <Footer/>
+      <Navbar />
+      <Hero />
+      <Brands />
+      <Features />
+      <Tools />
+      <Themes />
+      <Plugins />
+      <Globe />
+      <Reviews />
+      <Blogs />
+      <Selling />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
+
+// random comment to check whether commits are getting fully signed or not
